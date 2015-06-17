7f8d2ea89f2c1ec3ca8f11129d4909000d06af69
Karolina <carolina-day@hotmail.com>
2015-06-17 23:21:06 +0200

    eee zmienilam cos

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -5,13 +5,11 @@ class ReadersController < InheritedResources::Base
    end
     
     def searching
-    #  @books = Book.includes(:writers).where( 'writer.name LIKE ?', "%#{params[:title]}%")
-      #  @books = Book.joins(:writers).where( 'writer.name LIKE ?', "%#{params[:title]}%")
        @books = Book.where( 'title LIKE ?', "%#{params[:title]}%")
        @writers = Writer.where( 'name LIKE ?', "%#{params[:title]}%")
        @writers.each do |writer|
-        @books = @books + writer.books
-        end
+       @books = @books + writer.books
+      end
     end
 
     def my_reservations
@@ -21,17 +19,7 @@ class ReadersController < InheritedResources::Base
     def my_borrows
       @borrows = Reservation.where({reader_id: params[:id], received: true})
     end
-
-   def deleteRes
-   end
-
-   def confirmDeleting
-    @delRes = Reservation.find(params[:resID])
-    @delRes.destroy
-   end
-  
-
-    
+ 
   helper_method :resource, :resource_name, :devise_mapping
   def resource_name
       :reader
@@ -47,16 +35,10 @@ class ReadersController < InheritedResources::Base
 
   def forgottenPassword
   end
-<<<<<<< HEAD
-
-=======
-    #def editProfile
-   # end
-  
-   def showBook
+  def showBook
     @book = Book.find(params[:id])
-   end
->>>>>>> origin/redirecting
+  end
+
   private
     def reader_params
       params.require(:reader).permit(:name, :lastname)
--- config/routes.rb
+++ config/routes.rb
@@ -2,68 +2,28 @@ Rails.application.routes.draw do
   devise_for :librarians, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :readers, :controllers => {:passwords => "passwords"}
+devise_for :readers, :controllers => {:passwords => "passwords"}
 
-<<<<<<< HEAD
-  get 'newBooks' => 'welcome#newBooks'
-  get 'about' => 'welcome#about'
-  get 'login' => 'welcome#login'
-  get 'register' => 'welcome#register'
-  get 'forgottenPassword' => 'welcome#passwordNewPage'
 
-  resources :books
-   get 'newBooks/:id' => 'books#show'
-=======
-  #get 'newBooks' => 'welcome#newBooks'
- # get 'about' => 'welcome#about'
+get 'newBooks' => 'welcome#newBooks'
+get 'about' => 'welcome#about'
 get 'login' => 'welcome#login'
 get 'register' => 'welcome#register'
 get 'forgottenPassword' => 'welcome#passwordNewPage'
 
 resources :books
-  get 'newBooks/:id' => 'books#show'
-# devise_for :readers do
-#  	  root :to => "readers#reader"
- # end
+get 'newBooks/:id' => 'books#show'
+
+get 'login' => 'welcome#login'
+get 'register' => 'welcome#register'
+get 'forgottenPassword' => 'welcome#passwordNewPage'
+
 resources :reservations
 
-  devise_for :readers, :controllers => {:passwords => "passwords"}
-  devise_for :librarians, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
->>>>>>> origin/redirecting
-
-  resources :reservations
-
-<<<<<<< HEAD
-  root 'welcome#index'
- 
- get 'reader/:id' => 'readers#searching'
-=======
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  # You can have the root of your site routed with "root"
 root 'welcome#index'
- 
-
-#root 'application#index'
-  #get 'readers/sign_in' => 'layouts#index'
  
 get 'reader/:id' => 'readers#searching'
 get 'reader/:id/books/:id_book' => 'readers#showBook'
-# get 'editProfile' => 'readers#editProfile'
- #get 'reader/:id/searching/:title' => 'books#searching'
-
-  #get 'reader/:id/:title' => 'books#searching'
- # get '/librarian/:id' => 'librarians#librarian'
- # get '/reader/params[:title]' => 'books#searching'
-
-=begin resources :books do
-	collection do
-		get 'searching'
-	end
-end
-=end
->>>>>>> origin/redirecting
 
 resources :readers do
 	member do 
@@ -75,13 +35,9 @@ resources :readers do
 	end
 end  
 
-
 get 'readers/:id/my_reservations' => 'readers#my_reservations'
 get 'readers/:id/my_reservations/showRes' => 'readers#showRes'
 get 'readers/:id/my_borrows/showBorrow' => 'readers#showBorrow'
 post 'books/:id/reserved' => 'books#reserved'
-
-
-  
 
 end
