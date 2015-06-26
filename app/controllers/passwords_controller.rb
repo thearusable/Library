524252c8f8b86782af14404dba76f91d07306557
Arek Szczepkowicz <arek.szczepkowicz@gmail.com>
2015-06-26 18:12:58 +0200

    fix kolejny

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,15 +1,15 @@
 class PasswordsController < Devise::PasswordsController
   #protected
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    login_path
-  end
+  #def after_sending_reset_password_instructions_path_for(resource_name)
+  #  login_path
+  #end
 
   # POST /resource/password
   def create
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
 
     if resource.errors.empty?
-      set_flash_message(:notice, :send_instructions) if is_navigational_format?
+      flash[:notice] = "Za chwilę otrzymasz emaila z instrukcją jak zresetować hasło."
       redirect_to login_path
     else
       flash[:alert] = "Nie znaleziono podanego adresu!"
