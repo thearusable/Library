584d0b66fa1f7f76fc4ec049bd00141af4411cec
carol77 <carolina-day@hotmail.com>
2015-06-27 19:09:17 +0200

    ...(na karola)

--- db/seeds/04_Writers_and_Books.rb
+++ db/seeds/04_Writers_and_Books.rb
@@ -5,7 +5,7 @@ BooksCount.times do |n|
     r.releaseDate = Faker::Date.backward(Random.rand(1000))
     r.publishingHouse = Faker::Company.name
     r.ISBN = Faker::Number.number(6)
-    r.image = File.open('/app/assets/images/0.png')
+    r.image = File.open("app/assets/images/#{rand(4)}.png")
     r.category_id =  Faker::Lorem.paragraph
     id = Random.rand(Category.all.count) + 1
     r.category_id = id
@@ -22,15 +22,10 @@ WritersCount.times do |n|
     r.name =  Faker::Name.name
     r.biography =  Faker::Lorem.paragraph
     r.nationality = Faker::Address.country
-    r.image = File.open('/app/assets/images/0.png')
-    #r.image = Faker::Avatar.image
-    #r.image = File.open(File.join(Rails.root, "/app/assets/images/0.png"))
-    #r.image = File.open("#{Rails.root}/app/assets/images/0.png")
-    #r.image = Image.create({ :asset => File.new("#{Rails.root}/path/to/somefile.jpg")})
+    r.image = File.open("app/assets/images/#{rand(4)}.png")
 
     r.books << Book.find(n+1)
     r.books << Book.find(n+1+(BooksCount/2))
-
     r.books << Book.find(Random.rand(BooksCount)+1)
     
     r.save!
