06420291206bbc865db217ef487f83c6f9b94e0b
Arek Szczepkowicz <arek.szczepkowicz@gmail.com>
2015-06-27 18:28:04 +0200

    ..

--- db/seeds/04_Writers_and_Books.rb
+++ db/seeds/04_Writers_and_Books.rb
@@ -5,7 +5,7 @@ BooksCount.times do |n|
     r.releaseDate = Faker::Date.backward(Random.rand(1000))
     r.publishingHouse = Faker::Company.name
     r.ISBN = Faker::Number.number(6)
-    r.image = File.open("#{Rails.root}/app/assets/images/1.png")
+    r.image = File.open("/app/assets/images/1.png")
     r.category_id =  Faker::Lorem.paragraph
     id = Random.rand(Category.all.count) + 1
     r.category_id = id
@@ -22,7 +22,8 @@ WritersCount.times do |n|
     r.name =  Faker::Name.name
     r.biography =  Faker::Lorem.paragraph
     r.nationality = Faker::Address.country
-    r.image = Faker::Avatar.image
+    r.image = File.open("/app/assets/images/1.png")
+    #r.image = Faker::Avatar.image
     #r.image = File.open(File.join(Rails.root, "/app/assets/images/0.png"))
     #r.image = File.open("#{Rails.root}/app/assets/images/0.png")
     #r.image = Image.create({ :asset => File.new("#{Rails.root}/path/to/somefile.jpg")})
