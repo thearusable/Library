fc77cbdbc16b8fa74b94a5f7abab2a33ea3818b6
Arek Szczepkowicz <arek.szczepkowicz@gmail.com>
2015-06-27 17:05:09 +0200

    poprawki w seed

--- db/seeds/04_Writers_and_Books.rb
+++ db/seeds/04_Writers_and_Books.rb
@@ -5,7 +5,7 @@ BooksCount.times do |n|
     r.releaseDate = Faker::Date.backward(Random.rand(1000))
     r.publishingHouse = Faker::Company.name
     r.ISBN = Faker::Number.number(6)
-    #r.image = File.open('app/assets/images/ruby.png')
+    r.image = File.open("#{Rails.root}/app/assets/images/#{rand(0..7)}.png")
     r.category_id =  Faker::Lorem.paragraph
     id = Random.rand(Category.all.count) + 1
     r.category_id = id
@@ -22,12 +22,15 @@ WritersCount.times do |n|
     r.name =  Faker::Name.name
     r.biography =  Faker::Lorem.paragraph
     r.nationality = Faker::Address.country
-    #r.image = File.open('app/assets/images/ruby.png')
+    r.image = Faker::Avatar.image
+    #r.image = File.open(File.join(Rails.root, "/app/assets/images/0.png"))
+    #r.image = File.open("#{Rails.root}/app/assets/images/0.png")
+    #r.image = Image.create({ :asset => File.new("#{Rails.root}/path/to/somefile.jpg")})
 
     r.books << Book.find(n+1)
     r.books << Book.find(n+1+(BooksCount/2))
 
     r.books << Book.find(Random.rand(BooksCount)+1)
     
-    r.save
+    r.save!
 end
